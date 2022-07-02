b68b42eeb1e948a17b4ae3861dd00794ab92fdae
hoetzgit <41108116+hoetzgit@users.noreply.github.com>
2022-07-02 13:40:12 +0200

    20220702_002

--- pwsWeiherhammer/scripts/add_customFields_to_database.sh
+++ pwsWeiherhammer/scripts/add_customFields_to_database.sh
@@ -4,8 +4,6 @@
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=solar_wetBulb --type=REAL
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=solar_windchill --type=REAL
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=solar_appTemp --type=REAL
-#sudo echo "y" | /home/weewx/bin/wee_database --add-column=sunshine_time --type=REAL
-#sudo echo "y" | /home/weewx/bin/wee_database --add-column=sunshine_hours --type=REAL
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=asky_cpu_fan --type=INTEGER
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=airDensity --type=REAL
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=thwIndex --type=REAL
@@ -13,3 +11,4 @@
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=vaporPressure --type=REAL
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=windPressure --type=REAL
 #sudo echo "y" | /home/weewx/bin/wee_database --add-column=sunshineDur --type=REAL
+#sudo echo "y" | /home/weewx/bin/wee_database --add-column=sunshineDurOriginal --type=REAL
