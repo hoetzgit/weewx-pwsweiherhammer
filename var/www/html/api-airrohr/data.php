996bab033414f3d583f1458c8c144604824f90d0
hoetzgit <41108116+hoetzgit@users.noreply.github.com>
2023-02-15 18:38:37 +0100

    Backup 20230215

--- var/www/html/api-airrohr/data.php
+++ var/www/html/api-airrohr/data.php
@@ -3,12 +3,18 @@
   $json = file_get_contents('php://input');
   $results = json_decode($json,true);
 
+  $data_file = "airrohr_results.json";
+  file_put_contents($data_file, json_encode($results));
+
   // copy sensor data values to values array
   $values = array();
   foreach ($results["sensordatavalues"] as $sensordatavalues)
   {
     $values[$sensordatavalues["value_type"]] = $sensordatavalues["value"];
   }
+
+  $data_file = "airrohr_values.json";
+  file_put_contents($data_file, json_encode($values));
 
   $data = array();
   $data["dateTime"] = intval($date->getTimestamp());
@@ -28,6 +34,17 @@
   if (isset($values["humidity"]))
   {
     $data["airrohr_outHumidity"] = floatval($values["humidity"]);
+  }
+  if (isset($values["signal"]))
+  {
+    $data["airrohr_sig"] = intval($values["signal"]);
+    if ($data["airrohr_sig"] <= -100) {
+      $data["airrohr_sig_percent"] = 0;
+    } else if ($data["airrohr_sig"] >= -50) {
+      $data["airrohr_sig_percent"] = 100;
+    } else {
+      $data["airrohr_sig_percent"] = 2 * ($data["airrohr_sig"] + 100);
+    }
   }
 
   $data_file = "api_airrohr.json";
